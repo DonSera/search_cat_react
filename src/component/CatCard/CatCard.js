8bab39ffe47d9f9fbaad02784a37dcd0a8289a29
DonSera <sera.2021@lifeoasis.com>
2021-09-07 18:27:49 +0900

    feat : cat을 띄우는 div 부분 css 만들기

--- src/component/CatCard/CatCard.js
+++ src/component/CatCard/CatCard.js
@@ -1,7 +1,9 @@
+import styles from './CatCard.module.css'
+
 function CatCard({url, nameArray}) {
     return(
-        <div className="cat-card">
-            <img src={url} alt="고양이" width="200" height="200"/>
+        <div className={styles["cat-card"]}>
+            <img className={styles["cat-card-img"]} src={url} alt="고양이"/>
             <p>{nameArray[0]}</p>
             <p>{nameArray[1]}</p>
         </div>
