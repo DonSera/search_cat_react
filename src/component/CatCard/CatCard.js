d7b9c69603ab02c1c57dbb06771dfbbb09c1258f
DonSera <sera.2021@lifeoasis.com>
2021-09-08 14:00:24 +0900

    feat : lazy가 true이면 data-src로 받게 만듦

--- src/component/CatCard/CatCard.js
+++ src/component/CatCard/CatCard.js
@@ -1,10 +1,13 @@
 import styles from './CatCard.module.css'
 
-function CatCard({url, engName, koName}) {
-    return(
+function CatCard({url, engName, koName, lazy, index}) {
+    return (
         <div className={styles["cat-card"]}>
-            <img className={styles["cat-card-img"]} src={url} alt="고양이"/>
-            <p>{engName}</p>
+            {lazy ?
+                <img className={styles["cat-card-lazy"]} data-src={url} alt="고양이"/>
+                : <img className={styles["cat-card-img"]} src={url} alt="고양이"/>
+            }
+            <p>{index+1 + ") " + engName}</p>
             <p>{koName}</p>
         </div>
     );
